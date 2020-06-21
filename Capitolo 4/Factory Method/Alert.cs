9fee11d5fad6f1475902c2576d758ed92f194981
zetanove <antonio.pelleriti@gmail.com>
2020-06-21 17:55:25 +0200

    esempio Factory Method

---
+++ Capitolo 4/Factory Method/Alert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Factory_Method
+{
+    public abstract class Alert
+    {
+        public abstract void Send();
+    }
+
+    public class SMSAlert : Alert
+    {
+        private string number;
+        private string text;
+
+        public SMSAlert(string n, string t)
+        {
+            this.number = n;
+            this.text = t;
+        }
+
+        public override void Send()
+        {
+            Console.WriteLine("Invio di SMS al numero: " + number);
+        }
+    }
+
+    public class EmailAlert: Alert
+    {
+        private string address;
+        private string body;
+        private string subject;
+        private bool isHtml;
+
+        public EmailAlert(string addr, string subj, string text, bool html)
+        {
+            this.address = addr;
+            this.subject = subj;
+            this.body = text;
+            this.isHtml = html;
+        }
+
+        public override void Send()
+        {
+            Console.WriteLine("Invio di Email a: " +address);
+        }
+    }
+}
---
+++ Capitolo 4/Factory Method/EmailMessenger.cs
@@ -0,0 +1,11 @@
+﻿namespace Factory_Method
+{
+    public class EmailMessenger : Messenger
+    {
+        public override Alert CreateAlert(string title, string msg, string recipient)
+        {
+            EmailAlert alert = new EmailAlert(recipient, title, msg, true);
+            return alert;
+        }
+    }
+}
