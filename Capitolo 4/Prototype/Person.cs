5b6fecebb94cb952b2ed24a1e109814ad7158f0c
zetanove <antonio.pelleriti@gmail.com>
2020-06-23 18:30:38 +0200

    prototype pattern

---
+++ Capitolo 4/Prototype/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Persona: Prototype
+    {
+        private string nome;
+
+        private string cognome { get; set; }
+
+        internal List<Contatto> Contatti;
+
+        public Persona(string n, string c)
+        {
+            nome = n;
+            cognome = c;
+            Contatti = new List<Contatto>();
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+        public string GetCognome()
+        {
+            return cognome;
+        }
+
+        public void SetNome(string n)
+        {
+            nome=n;
+        }
+        public void SetCognome(string c)
+        {
+            cognome=c;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{nome} {cognome} - hashcode: {this.GetHashCode()}");
+            foreach (var c in Contatti)
+                sb.AppendLine(c.ToString());
+
+            return sb.ToString();
+        }
+
+        
+        public Prototype Clone()
+        {
+            //Shallow clone, i contatti rimangono per riferimento
+            //return (Person)this.MemberwiseClone();
+
+
+            //deep
+            Persona clone = (Persona)this.MemberwiseClone();
+            clone.Contatti=new List<Contatto>();
+            foreach (var c in this.Contatti)
+                clone.Contatti.Add((Contatto)c.Clone());
+
+            return clone;
+                
+        }
+    }
+
+    enum ContactType
+    {
+        Tel,
+        Email,
+        Fax,
+        Social,
+    }
+
+     class Contatto: Prototype
+    {
+        public ContactType Type;
+        public string Text;
+
+        public Contatto(ContactType type)
+        {
+            this.Type = type;
+        }
+
+        public Contatto(ContactType type, string text)
+        {
+            Type = type;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} {Text} - hashcode: {this.GetHashCode()}";
+        }
+
+        public Prototype Clone()
+        {
+            return (Contatto)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Capitolo 4/Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Antonio", "Pelleriti");
+            p1.Contatti.Add(new Contatto(ContactType.Email, "antonio.pelleriti@gmail.com"));
+
+            Console.WriteLine(p1);
+
+            Persona p2 = p1;
+            p2.SetNome( "Anto");
+            Console.WriteLine(p2);
+            Console.WriteLine(p1);
+
+
+            Persona p3 = (Persona)p1.Clone();
+            p3.Contatti[0].Text = "anto@outlook.com";
+            Console.WriteLine(p1);
+            Console.WriteLine(p3);
+            Console.Read();
+        }
+    }
+}
