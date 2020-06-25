14e5b0ca99ad8ddf95c849a41721d8646b02008d
zetanove <supportoinformatico@confimprenditori.it>
2020-06-25 17:48:11 +0200

    esempio adapter

---
+++ Structural Patterns/ADapter/Adapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    interface ITarget
+    {
+        void Request();
+    }
+
+
+    class Adapter: ITarget
+    {
+        public Adapter(Adaptee aee)
+        {
+            adaptee = aee;
+        }
+
+        private Adaptee adaptee;
+
+        public void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Adaptee.SpecificRequest");
+        }
+    }
+}
---
+++ Structural Patterns/ADapter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee aee = new Adaptee();
+            //Test(aee); //non va, serve un adattatore
+
+            ITarget target = new Adapter(aee);
+
+            Test(target);
+        }
+
+        static void Test(ITarget t)
+        {
+            t.Request();
+        }
+    }
+}
