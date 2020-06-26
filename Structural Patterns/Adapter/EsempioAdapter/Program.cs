4853e5571d4877bb66e835d14eb6a5b3324c619d
zetanove <supportoinformatico@confimprenditori.it>
2020-06-26 16:50:12 +0200

    esempi pattern Adapter

---
+++ Structural Patterns/Adapter/EsempioAdapter/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsempioAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintBadge("180777");
+        }
+
+        public static void PrintBadge(string matricola)
+        {
+            IPerson person = null;
+            
+            //person = new Impiegato(){ Matricola = matricola}; //carica dal database
+            person = new RemoteServiceAdapter();
+            person.Matricola = matricola;
+
+            string[] data = person.GetBadgeData();
+            Badge b=new Badge(data);
+            var image=b.ExportAsImage();
+        }
+
+
+    }
+
+    class Badge
+    {
+        public Badge(string[] data)
+        {
+            
+        }
+
+        public string ExportAsImage()
+        {
+            return "image";
+        }
+    }
+
+    interface IPerson
+    {
+        string Matricola { get; set; }
+        string[] GetBadgeData();
+    }
+
+    class Impiegato : IPerson
+    {
+        public string Matricola { get; set; }
+
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public string Foto { get; set; }
+
+        public string[] GetBadgeData()
+        {
+            //restituisce i dati necessari alla creazione del badge
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class RemoteServiceAdapter : IPerson
+    {
+        public string Matricola { get ; set ; }
+
+        private HRService service;
+
+        public RemoteServiceAdapter()
+        {
+            service=new HRService();
+        }
+
+        public string[] GetBadgeData()
+        {
+            var json= service.GetHumaResource(Matricola);
+            //ricava i dati dal json...
+            string[] data = new string[]{Matricola,"nome","cognome","urlphoto"};
+
+            return data;
+
+        }
+    }
+
+    public class HRService
+    {
+        public string GetHumaResource(string matricola)
+        {
+            return "json";
+        }
+    }
+
+    
+}
---
+++ Structural Patterns/Class Adapter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget target = new Adapter();
+            Test(target);
+        }
+
+        static void Test(ITarget t)
+        {
+            t.Request();
+        }
+    }
+
+    interface ITarget
+    {
+        void Request();
+    }
+
+
+    class Adapter : Adaptee, ITarget
+    {
+        public Adapter()
+        {
+            
+        }
+
+        public void Request()
+        {
+            base.SpecificRequest();
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Adaptee.SpecificRequest");
+        }
+    }
+}
