7b873448d6af987aec6948efc9ff86fa71a875b9
zetanove <supportoinformatico@confimprenditori.it>
2020-06-29 17:49:53 +0200

    esempio composite

---
+++ Structural Patterns/Composite UI/Form.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Composite_UI
+{
+    public class Form : UIContainer
+    {
+        public Form(string name)
+        {
+            this.name = name;
+        }
+
+        public override string Render()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("<Form Name='{0}'>", name);
+            builder.AppendLine();
+
+            foreach (var component in children)
+            {
+                builder.Append(component.Render());
+            }
+
+            builder.AppendLine("</Form>");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Structural Patterns/Composite UI/Panel.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Composite_UI
+{
+    public class Panel : UIContainer
+    {
+        public string BackgroundColor;
+
+        public Panel(string name)
+        {
+            this.name = name;
+            BackgroundColor = "#ffffff";
+        }
+
+        public override string Render()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("  <Panel Name='{0}' Background='{1}'>", name, BackgroundColor);
+            builder.AppendLine();
+
+            foreach (var component in children)
+            {
+                builder.AppendLine(component.Render());
+            }
+
+            builder.AppendLine("  </Panel>");
+
+            return builder.ToString();
+        }
+    }
+}
