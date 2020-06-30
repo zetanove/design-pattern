04971222c25ac75ce4c9c801f375847caab603d8
zetanove <antonio.pelleriti@gmail.com>
2020-07-01 00:28:43 +0200

    esempi decorator da finire

---
+++ Structural Patterns/Decorator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log=new ConsoleLog()
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(String msg);
+    }
+
+    public abstract class Logger : ILogger
+    {
+        public ILogger Component { get; set; }
+        public abstract void Log(string msg);
+        
+        public void SetComponent(ILogger il)
+        {
+            Component = il;
+        }
+    }
+
+    public class ConsoleLog : Logger
+    {
+        public override void Log(string msg)
+        {
+            this.Component.Log(msg);
+            Console.WriteLine(msg);
+        }
+    }
+
+    class FileLog : Logger
+    {
+        public override void Log(string msg)
+        {
+            this.Component.Log(msg);
+            //throw new NotImplementedException();
+        }
+
+
+    }
+}
