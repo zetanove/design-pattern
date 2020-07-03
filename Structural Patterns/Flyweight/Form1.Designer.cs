6f97194dea73c9610a219349a9a37512a20a7f0e
zetanove <antonio.pelleriti@gmail.com>
2020-07-04 00:38:05 +0200

    esempi flyweight

--- Structural Patterns/Flyweight/Form1.Designer.cs
+++ Structural Patterns/Flyweight/Form1.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.labTotIstanze = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(799, 26);
+            this.button1.Location = new System.Drawing.Point(709, 26);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(165, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Crea 100 figure random";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel1
             // 
@@ -49,17 +51,28 @@
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // labTotIstanze
+            // 
+            this.labTotIstanze.AutoSize = true;
+            this.labTotIstanze.Location = new System.Drawing.Point(33, 26);
+            this.labTotIstanze.Name = "labTotIstanze";
+            this.labTotIstanze.Size = new System.Drawing.Size(65, 13);
+            this.labTotIstanze.TabIndex = 2;
+            this.labTotIstanze.Text = "Tot. istanze:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 590);
+            this.Controls.Add(this.labTotIstanze);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labTotIstanze;
     }
 }
 
--- Structural Patterns/Flyweight/Form1.cs
+++ Structural Patterns/Flyweight/Form1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,13 @@ namespace Flyweight
 {
     public partial class Form1 : Form
     {
+        public List<IShape> Shapes = new List<IShape>();
+        ShapeFactory factory;
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
+            factory = new ShapeFactory();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,35 +30,155 @@ namespace Flyweight
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawEllipse(Pens.Red, 100,100,50,50);
+            foreach (var shape in Shapes)
+            {
+                if (shape is Circle circle)
+                {
+                    circle.Move(rnd.Next(0, this.panel1.Width), rnd.Next(0, this.panel1.Height));
+                    e.Graphics.FillEllipse(new SolidBrush(Color.FromName(circle.Color)),
+                        circle.Position.X, 
+                        circle.Position.Y, 
+                        circle.Radius*2,  
+                        circle.Radius*2);
+                }
+                else if(shape is Square square)
+                {
+                    square.Move(rnd.Next(0, this.panel1.Width), rnd.Next(0, this.panel1.Height));
+                    e.Graphics.FillRectangle(new SolidBrush(Color.FromName(square.Color)),
+                        square.Position.X,
+                        square.Position.Y,
+                        square.Width,
+                        square.Width);
+                }
+            }
         }
-    }
 
-    public class CircleFactory
-    {
-        
-    }
-
-    public class Circle
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-
-    }
-
-    public class CircleFlyweight
-    {
-        private Circle _sharedState;
-
-        public CircleFlyweight(Circle c)
+        private void button1_Click(object sender, EventArgs e)
         {
-            this._sharedState = c;
-        }
+            List<string> colors = new List<string>() { "Red", "Blue", "Yellow" };
+            for (int i = 0; i < 100; i++)
+            {
+                if (rnd.Next(1,19)>5)
+                {
+                    Circle c = (Circle)factory.GetShape(typeof(Circle), colors.OrderBy(g => Guid.NewGuid()).First());
+                    c.Radius = 10;
+                    Shapes.Add(c);
+                }
+                else
+                {
+                    Square c = (Square)factory.GetShape(typeof(Square), colors.OrderBy(g => Guid.NewGuid()).First());
+                    c.Width = 10;
+                    Shapes.Add(c);
+                }
+                
+            }
 
-        public void Move(Circle newState)
+            labTotIstanze.Text = $"Figure totali: {Shapes.Count} - Istanze : {factory.Count}";
+
+            panel1.Invalidate();
+        }
+    }
+}
+
+//Flyweight Factory
+public class ShapeFactory
+{
+    Dictionary<string, IShape> _shapes = new Dictionary<string, IShape>();
+
+    public int Count
+    {
+        get
         {
-
+            return _shapes.Count;
         }
+    }
+
+    public IShape GetShape(Type shapeType, string color)
+    {
+        string type = shapeType.Name;
+        if (_shapes.ContainsKey(color + type))
+        {
+            return _shapes[color + type];
+        }
+        else
+        {
+            if (shapeType == typeof(Circle))
+            {
+                Circle c = new Circle(color);
+                _shapes.Add(color + type, c);
+                return c;
+            }
+            else if (shapeType == typeof(Square))
+            {
+                Square s = new Square(color);
+                _shapes.Add(color + type, s);
+                return s;
+            }
+        }
+
+        return null;
+
+    }
+}
+
+    //Flyweight interface
+    public interface IShape
+    {
+        void Move(int x, int y);
+    }
+
+//concrete fly weight
+public class Circle : IShape
+{
+    //stato intrinseco
+    private string color;
+
+    public string Color
+    {
+        get
+        {
+            return color;
+        }
+    }
+
+    public int Radius;
+    public Point Position;
+
+    public Circle(string c)
+    {
+        this.color = c;
+    }
+
+    public void Move(int x, int y)
+    {
+        this.Position = new Point(x, y);
+    }
+}
+
+public class Square : IShape
+{
+
+    //stato intrinseco
+    private string color;
+
+    public string Color
+    {
+        get
+        {
+            return color;
+        }
+    }
+
+    public int Width;
+    public Point Position;
+
+    public Square(string c)
+    {
+        this.color = c;
+    }
+
+    public void Move(int x, int y)
+    {
+        this.Position = new Point(x, y);
     }
 }
