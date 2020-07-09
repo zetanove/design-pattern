b0e030108313b0dcb963807474a22430cc5d7d7b
zetanove <supportoinformatico@confimprenditori.it>
2020-07-09 16:52:41 +0200

    esempio chain of responsibility

--- Behavioral Patterns/Chain Of Responsibility/Program.cs
+++ Behavioral Patterns/Chain Of Responsibility/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,32 @@ namespace Chain_Of_Responsibility
     {
         static void Main(string[] args)
         {
-            Logger console=new ConsoleLogger(LogLevel.DEBUG);
-            Logger error=new ErrorLogger(LogLevel.ERROR);
+            Logger consoleLogger = new ConsoleLogger(LogLevel.DEBUG);
+            Logger fileLogger = new FileLogger("log.txt", LogLevel.INFO);
+            Logger warningLogger = new WarningLogger(LogLevel.WARNING);
+            Logger emailLogger = new MailLogger("test@email.it", LogLevel.ERROR);
 
-            console.SetNextHandler(error);
+            consoleLogger.SetNextHandler(fileLogger);
+            fileLogger.SetNextHandler(warningLogger);
+            warningLogger.SetNextHandler(emailLogger);
 
-            Logger chain = console;
+            consoleLogger
+                .SetNextHandler(fileLogger)
+                .SetNextHandler(warningLogger)
+                .SetNextHandler(emailLogger);
 
+            consoleLogger = new ConsoleLogger(LogLevel.DEBUG,
+                                new FileLogger("log.txt", LogLevel.INFO,
+                                    new WarningLogger(LogLevel.WARNING,
+                                        new MailLogger("test@email.it", LogLevel.ERROR)
+                                    )
+                                )
+                            );
+            Logger chain = consoleLogger;
 
             chain.Log("Questa è una informazione", LogLevel.DEBUG);
+            chain.Log("log info", LogLevel.INFO);
+            chain.Log("log di un avviso", LogLevel.WARNING);
             chain.Log("Questo è un errore", LogLevel.ERROR);
         }
     }
@@ -36,7 +54,7 @@ namespace Chain_Of_Responsibility
     {
         void Log(string msg, LogLevel level);
 
-        void SetNextHandler(Logger next);
+        Logger SetNextHandler(Logger next);
     }
 
     public abstract class AbstractLogger: Logger
@@ -44,56 +62,111 @@ namespace Chain_Of_Responsibility
         private Logger nextHandler;
         protected LogLevel _level;
 
-        public AbstractLogger(LogLevel level)
+        public AbstractLogger(LogLevel level): this(level, null)
         {
-            this._level = level;
         }
 
-        public void SetNextHandler(Logger next)
+        public AbstractLogger(LogLevel level, Logger next)
+        {
+            this._level = level;
+            this.nextHandler = next;
+        }
+
+        public Logger SetNextHandler(Logger next)
         {
             this.nextHandler = next;
+            return next;
         }
 
         public void Log(string msg, LogLevel level)
         {
-            if (this._level <= level)
+            if (this._level == level)
             {
-                TraceLog(msg);
+                CreateLog(msg);
             }
-            
-            if (nextHandler != null)
+            else if (nextHandler != null)
             {
                 nextHandler.Log(msg, level);
             }
         }
 
-        protected abstract void TraceLog(string message);
+        protected abstract void CreateLog(string message);
 
     }
 
     public class ConsoleLogger : AbstractLogger
     {
-        public ConsoleLogger(LogLevel level): base(level)
+        public ConsoleLogger(LogLevel level) : this(level, null)
         {
         }
 
-        protected override void TraceLog(string message)
+        public ConsoleLogger(LogLevel level, Logger next) : base(level, next)
+        {
+        }
+
+
+        protected override void CreateLog(string message)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(this._level+": "+message);
+            Console.WriteLine(this.GetType().Name+" - "+this._level+": "+message);
         }
     }
 
-    public class ErrorLogger : AbstractLogger
+    public class WarningLogger : AbstractLogger
     {
-        public ErrorLogger(LogLevel level) : base(level)
+        public WarningLogger(LogLevel level, Logger next) : base(level, next)
         {
         }
 
-        protected override void TraceLog(string message)
+        public WarningLogger(LogLevel level) : this(level, null)
+        {
+        }
+
+        protected override void CreateLog(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(this._level + ": " + message);
+            Console.WriteLine(this.GetType().Name + " - " + this._level + ": " + message);
+        }
+    }
+
+    public class FileLogger : AbstractLogger
+    {
+        private string filePath;
+
+        public FileLogger(string path, LogLevel level, Logger next) : base( level, next)
+        {
+            filePath = path;
+        }
+
+        public FileLogger(string path, LogLevel level) : this(path, level, null)
+        {
+        }
+
+        protected override void CreateLog(string message)
+        {
+            Console.WriteLine(this.GetType().Name + " - " + this._level + ": " + message);
+            File.AppendAllText(filePath, message);
+        }
+    }
+
+    public class MailLogger : AbstractLogger
+    {
+        private string _email;
+
+        public MailLogger(string email, LogLevel level, Logger next) : base(level, next)
+        {
+            _email = email;
+        }
+
+        public MailLogger(string email, LogLevel level) : this(email, level, null)
+        {
+        }
+
+        protected override void CreateLog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;            
+            Console.WriteLine(this.GetType().Name + " - " + this._level + ": " + message); 
+            Console.WriteLine("invio mail di log a: " + _email);
         }
     }
 }
