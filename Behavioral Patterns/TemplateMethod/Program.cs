d467487b3bf4cbf6ac34f16114aece9e28cab9e1
zetanove <supportoinformatico@confimprenditori.it>
2020-07-23 18:01:20 +0200

    template method da finire

---
+++ Behavioral Patterns/TemplateMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public abstract class AbstractSearch
+    {
+        public void ReadTable()
+        {
+
+        }
+
+        public abstract void ExportToFile(string file)
+        {
+
+        }
+    }
+
+    class GoogleSearch: AbstractSearch
+    {
+        public override void ExportToFile(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
