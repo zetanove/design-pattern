aee45d5d46f278a38bd8cd4361130f356b1cae5f
zetanove <supportoinformatico@confimprenditori.it>
2020-07-24 17:51:56 +0200

    template method

--- Behavioral Patterns/TemplateMethod/Program.cs
+++ Behavioral Patterns/TemplateMethod/Program.cs
@@ -10,29 +10,67 @@ namespace TemplateMethod
     {
         static void Main(string[] args)
         {
+            PDFExport pdf = new PDFExport();
+            pdf.ExportData("clienti","clienti.pdf");
+
+            Console.ReadLine();
         }
     }
 
 
-    public abstract class AbstractSearch
+    public abstract class AbstractExporter
     {
-        public void ReadTable()
+        public void ExportData(string table, string path)
         {
+            OpenConnection();
+            ReadData(table);
+            ExportToFile(path);
+            CloseConnection();
 
+            if (MustLog())
+                Log();
         }
 
-        public abstract void ExportToFile(string file)
+        private void OpenConnection()
         {
-
+            Console.WriteLine("AbstractClass si connette al database...");
         }
+
+        private void CloseConnection()
+        {
+            Console.WriteLine("AbstractClass chiude la connessione.");
+        }
+
+        protected virtual bool MustLog()
+        {
+            return false;
+        }
+
+        protected virtual void Log()
+        {
+        }      
+
+        protected void ReadData(string table)
+        {
+            Console.WriteLine($"AbstractClass legge i dati dalla tabella {table}");
+        }
+
+        protected abstract void ExportToFile(string file);
     }
 
-    class GoogleSearch: AbstractSearch
+    public class PDFExport: AbstractExporter
     {
-        public override void ExportToFile(string file)
+        protected override void ExportToFile(string file)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"PDFExport: Esporto i dati sul file {file}");
         }
+
+        protected override bool MustLog()
+        {
+            return base.MustLog();
+        }
+
     }
+
 
 }
